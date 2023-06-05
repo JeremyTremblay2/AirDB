feeee991104b0db10bf5aad25d042057a7c4b06d
etudiant <etudiant@MB-R-110-001-176.local>
2023-06-05 09:03:25 +0200

    [#12] Init Springboot project

---
+++ src/server/AirDB/src/main/java/com/example/AirDB/AirDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.AirDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirDbApplication.class, args);
+	}
+
+}
---
+++ src/server/AirDB/src/test/java/com/example/AirDB/AirDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AirDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
